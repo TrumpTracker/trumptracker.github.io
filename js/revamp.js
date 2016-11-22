1572a477a5f6e2fb1075f822c38e7c14fe0acb82
Chris Bloom <bloomcb@gmail.com>
2016-11-22 13:25:44 -0800

    Js filtering work started

--- js/revamp.js
+++ js/revamp.js
@@ -1,5 +1,26 @@
-var listOptions = {
-  valueNames: ['promise-text']
-};
+(function($) {
 
-var promiseList = new List('promises', listOptions);
+  var listOptions = {
+    valueNames: [
+      'js-promise-text',
+      'js-promise-category',
+      'js-promise-status'
+    ]
+  };
+
+  $(function() {
+
+    // List.js object that we can filter upon
+    var promiseList = new List('promises', listOptions);
+
+    $('.promises__category--reset').on('click', function(e) {
+      $('.promises__category', '.promises__categories').removeClass('active');
+    });
+
+    $('.promises__categories').on('click', '.promises__category', function(e) {
+      $(this).toggleClass('active');
+    });
+
+  });
+
+})(jQuery);
