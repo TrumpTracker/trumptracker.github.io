45e36396da22fea74335c9614f96b6f77f9b8d39
Chris Bloom <bloomcb@gmail.com>
2016-11-25 14:07:48 -0800

    Closer to more robust filtering

--- js/revamp.js
+++ js/revamp.js
@@ -13,8 +13,8 @@
     // List.js object that we can filter upon
     var promiseList = new List('promises', listOptions);
 
-    var $search = $('#search');
-    var $facets = $('[data-list-facet]');
+    var $search = $('#search'); // The form
+    var $facets = $('[data-list-facet]'); // All buttons that can filter
 
     // Clear all
     function resetFilter(e) {
@@ -26,20 +26,30 @@
       promiseList.filter();
     }
 
+    // Hard reset all the buttons
     $('.promises__category--reset').on('click', resetFilter);
 
     // Any facet filter button
     $facets.on('click', function(e) {
+      var facet = $(this).data('list-facet'); // ie 'js-promise-category'
+      var value = $(this).data('facet-value'); // ie 'Culture'
+
+      // Single-select categories should have their active state wiped
+      if ($(this).data('select-single')) {
+        $facets
+          .filter(function() { return $(this).data('list-facet') === facet; })
+          .removeClass('active');
+      }
 
       // Flag as active
       $(this).toggleClass('active');
 
       // Array of active
-      var actives = $('[data-list-facet].active').map(function() {
+      var actives = $facets.filter('.active').map(function() {
         // return object instead with facet/value
         return {
-          facet: $(this).data('list-facet'),
-          value: $(this).data('facet-value')
+          facet: facet,
+          value: value
         };
       }).get();
 
@@ -49,30 +59,28 @@
       if (actives.length === 0) {
         // resetFilter();
         promiseList.filter();
+        return; // Eject now
       }
 
       // Otherwise, filter on the array
-      else {
-        promiseList.filter(function(item) {
+      promiseList.filter(function(item) {
 
-          // For all active filters, just one needs to flag as true to for entire reduce here to be true
-          return actives.reduce(function(found, current) {
 
-            // found === true, then skip
-            if (found) {
-              return found;
-            }
 
-            return item.values()[current.facet] === current.value;
+        // For all active filters, just one needs to flag as true to for entire reduce here to be true
+        return actives.reduce(function(found, current) {
+          // found === true, then skip
+          // if (found) {
+          //   return found;
+          // }
 
-          }, false); // Start reduce at false
+          return item.values()[current.facet] === current.value;
 
-        //   return (actives.indexOf(item.values()[facet]) !== -1);
-        }); // promiseList.filter()
-      }
+        }, false); // Start reduce at false
+
+      }); // promiseList.filter()
 
     });
-
   });
 
 })(jQuery);
