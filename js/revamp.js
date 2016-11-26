3a63e2ebe0c7c2a359e3df0869c0d44c336ac9b9
Chris Bloom <bloomcb@gmail.com>
2016-11-26 10:54:13 -0800

    Transitioning

--- js/revamp.js
+++ js/revamp.js
@@ -33,9 +33,10 @@
     $facets.on('click', function(e) {
       var facet = $(this).data('list-facet'); // ie 'js-promise-category'
       var value = $(this).data('facet-value'); // ie 'Culture'
+      var isSingle = !!$(this).data('select-single'); // ie true/false for if there can only be one of this filter
 
       // Single-select categories should have their active state wiped
-      if ($(this).data('select-single')) {
+      if (isSingle) {
         $facets
           .filter(function() { return $(this).data('list-facet') === facet; })
           .removeClass('active');
@@ -45,19 +46,19 @@
       $(this).toggleClass('active');
 
       // Array of active
-      var actives = $facets.filter('.active').map(function() {
+      var facets = $facets.filter('.active').map(function() {
         // return object instead with facet/value
         return {
-          facet: facet,
-          value: value
+          facet: $(this).data('list-facet'),
+          value: $(this).data('facet-value'),
+          isSingle: !!$(this).data('select-single')
         };
       }).get();
 
-      console.log(actives);
+      console.log(facets);
 
       // When deselecting last, clear all filters
-      if (actives.length === 0) {
-        // resetFilter();
+      if (facets.length === 0) {
         promiseList.filter();
         return; // Eject now
       }
@@ -65,18 +66,51 @@
       // Otherwise, filter on the array
       promiseList.filter(function(item) {
 
-
+        var vals = item.values();
 
         // For all active filters, just one needs to flag as true to for entire reduce here to be true
-        return actives.reduce(function(found, current) {
-          // found === true, then skip
-          // if (found) {
-          //   return found;
-          // }
+        var singles = facets.reduce(function(found, facet) {
 
-          return item.values()[current.facet] === current.value;
+          return facet.isSingle && vals[facet.facet] === facet.value;
+        }, false);
 
-        }, false); // Start reduce at false
+        var multis = facets.reduce(function(found, facet) {
+          if (found) {
+            return found;
+          }
+          return !facet.isSingle && vals[facet.facet] == facet.value;
+        }, false);
+
+        if (singles) {
+          return singles;
+        }
+
+        if (multis) {
+          return multis;
+        }
+
+        // console.log(singles);
+        // console.log(multis);
+
+        // return facets.reduce(function(found, facet) {
+        //
+        //   // console.log(vals[facet.facet]);
+        //   // console.log(facet.value);
+        //
+        //   if (facet.isSingle && vals[facet.facet] !== facet.value) {
+        //     return false;
+        //   }
+        //
+        //   // found === true, then skip
+        //   // if (found) {
+        //   //   return found;
+        //   // }
+        //   if (!facet.isSingle && vals[facet.facet] === facet.value) {
+        //     return true;
+        //   }
+        //   // return vals[facet.facet] === facet.value;
+        //
+        // }, true); // Start reduce at false
 
       }); // promiseList.filter()
 
