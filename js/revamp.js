b32ff727c1a6f6565dffada5e0da8805763d8cb2
Christopher Bloom <bloomcb@gmail.com>
2017-01-10 11:34:01 -0500

    Ui/data refresh (#66)
    * Updates to markup to use partials, reassign variables to make it clearer
    
    * Progress on promise structure
    
    * Start of table work
    
    * New promises table
    
    * Moving work to a revamp yaml/html file instead of in index
    
    * Revert index to current state of master
    
    * Revert page.html to upstream master
    
    * Revamp on data structure
    
    * Culture revamp
    
    * Update with list.js filtering to promises
    
    * Js filtering work started
    
    * Update filtering
    
    * Reset if no categories selected
    
    * Filter approaches for multiple facets
    
    * Work prior to 'or' groups
    
    * Closer to more robust filtering
    
    * Transitioning
    
    * Much cleaner filters
    
    * Continue revamp on data architecture
    
    * Complet the revamp on yml
    
    * Move the 3 items in 'Indigenous' to other categories for space for nwo
    
    * Remove scoreboard partial, cleanup on revamp files before PR
    
    * Remove changes to original page.html
    
    * Added moment.js and status looping because why not
    
    * Closure around moment
    
    * Icon for statuses within table
    
    * days in office logic
    
    * Remove jsonified promise data and pull in fontawesome css instead of js version

---
+++ js/revamp.js
@@ -0,0 +1,113 @@
+(function($, List, _, moment) {
+
+  // List.js classes to use for search elements
+  var listOptions = {
+    valueNames: [
+      'js-promise-text',
+      'js-promise-category',
+      'js-promise-status'
+    ]
+  };
+
+  // Find any within a facet
+  function foundAny(facets, compareItem) {
+    // No facets selected, show all for this facet
+    if (_.isEmpty(facets)) {
+      return true;
+    }
+    // Otherwise, show this item if it contains any of the selected facets
+    return facets.reduce(function(found, facet) {
+      if (found) {
+        return found;
+      }
+      return compareItem[facet['facet']] === facet['value'];
+    }, false);
+  }
+
+  $(function() {
+
+    // Dates
+    var today = moment();
+    var inauguration = moment('2017-01-20');
+    $('#inauguration-days').html(inauguration.diff(today, 'days') > 0 ? inauguration.diff(today, 'days') : 'NA');
+    $('#days-in-office').html(today.diff(inauguration, 'days') > 0 ? today.diff(inauguration, 'days') : 0);
+
+    // List.js object that we can filter upon
+    var promiseList = new List('promises', listOptions).on('updated', function(list) {
+      $('#count').html(list.visibleItems.length);
+    });
+
+    var $search = $('#search');
+    var $facets = $('[data-list-facet]'); // All buttons that can filter
+
+    // Clear all
+    function resetFilter(e) {
+      e.preventDefault();
+      // Visually reset buttons
+      $facets.removeClass('active');
+      // Clear out text field
+      $search.val('');
+      // Wipe all filters
+      promiseList.search();
+      promiseList.filter();
+    }
+
+    // Hard reset all the buttons
+    $('.promises__category--reset').on('click', resetFilter);
+
+    // Any facet filter button
+    $facets.on('click', function(e) {
+
+      var facet = $(this).data('list-facet'); // ie 'js-promise-category'
+      var value = $(this).data('facet-value'); // ie 'Culture'
+      var isSingle = !!$(this).data('select-single'); // ie true/false for if there can only be one of this filter
+
+      // Single-select categories should have their active state wiped
+      if (isSingle) {
+        $facets
+          .filter(function() { return $(this).data('list-facet') === facet; })
+          .removeClass('active');
+      }
+
+      // Flag as active
+      $(this).toggleClass('active');
+
+      // Array of active
+      var facets = $facets.filter('.active').map(function() {
+        // return object instead with facet/value
+        return {
+          facet: $(this).data('list-facet'),
+          value: $(this).data('facet-value'),
+          isSingle: !!$(this).data('select-single')
+        };
+      }).get();
+
+      // When deselecting last, clear all filters
+      if (facets.length === 0) {
+        promiseList.filter();
+        return; // Eject now
+      }
+
+      // Otherwise, filter on the array
+      promiseList.filter(function(item) {
+
+        var itemValues = item.values();
+
+        // Single selects, eg "Not started"
+        var single = _.filter(facets, ['isSingle', true]);
+        var foundSingle = foundAny(single, itemValues);
+        // Single-selection items hide if false no matter what, so eject if not found here
+        if (!foundSingle) {
+          return false;
+        }
+
+        // Full categories can have multiples show, list out here
+        var multis = _.filter(facets, ['isSingle', false]);
+        return foundAny(multis, itemValues);
+
+      }); // promiseList.filter()
+
+    });
+  });
+
+})(jQuery, List, _, moment);
