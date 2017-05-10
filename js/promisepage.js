61749d6a06d47fc9b1a7c0337a895dfb7bd00fe8
MrLuit <me@luithollander.nl>
2017-05-10 17:39:42 +0200

    Fixed tracking noscript and removed reddit library

--- js/promisepage.js
+++ js/promisepage.js
@@ -1,10 +1,9 @@
 function renderComment(reddit, reply = false) {
-	if(reply) {
-		comment = "<div class='panel panel-default replycomment'><div class='panel-body'><i class='fa reply fa-reply' aria-hidden='true'></i> ";
-	}
-	else {
-		comment = "<div class='panel panel-default'><div class='panel-body'>";
-	}
+    if (reply) {
+        comment = "<div class='panel panel-default replycomment'><div class='panel-body'><i class='fa reply fa-reply' aria-hidden='true'></i> ";
+    } else {
+        comment = "<div class='panel panel-default'><div class='panel-body'>";
+    }
     if (reddit.data.distinguished != null) {
         comment += "<i class='fa fa-check-square-o check' aria-hidden='true'></i> ";
     }
@@ -24,30 +23,35 @@ function renderComment(reddit, reply = false) {
     if (reddit.data.edited != false) {
         comment += "*";
     }
-    comment += " <a target='_blank' href='https://reddit.com/r/trumptracker/comments/" + reddit.data.link_id.replace("t3_", "") + "/trumptracker/" + reddit.data.id + "/'><i class='fa fa-share-alt' aria-hidden='true'></i></a> <hr>" + SnuOwnd.getParser().render(reddit.data.body.replace(/(<([^>]+)>)/ig,""));
+    comment += " <a target='_blank' href='https://reddit.com/r/trumptracker/comments/" + reddit.data.link_id.replace("t3_", "") + "/trumptracker/" + reddit.data.id + "/'><i class='fa fa-share-alt' aria-hidden='true'></i></a> <hr>" + SnuOwnd.getParser().render(reddit.data.body.replace(/(<([^>]+)>)/ig, ""));
     if (reddit.data.archived != false) {
         comment += " <i class='fa fa-archive archive' aria-hidden='true'></i>";
     }
-	comment += "</div></div>";
+    comment += "</div></div>";
     return comment;
 }
+
+var nocomments = true;
 
 function loopComments(reddit, reply = false) {
     if (!reply) {
         $.each(reddit, function(i, data) {
             $.each(data, function(i, d) {
-                $.each(d.children, function(i, e) {
-                    $(".loader").hide();
-                    $(".panel-group").append(renderComment(e));
-                    if (e.data.replies != '') {
-                        loopComments(e.data.replies, true);
-                    }
-                });
+                if (d.hasOwnProperty("children")) {
+                    $.each(d.children, function(i, e) {
+                        nocomments = false;
+                        $(".loader").hide();
+                        $(".panel-group").append(renderComment(e));
+                        if (e.data.replies != '') {
+                            loopComments(e.data.replies, true);
+                        }
+                    });
+                }
             });
         });
     } else {
         $.each(reddit.data.children, function(i, e) {
-            $(".panel-group").append(renderComment(e,true));
+            $(".panel-group").append(renderComment(e, true));
             if (e.data.replies != '') {
                 loopComments(e.data.replies, true);
             }
@@ -55,20 +59,22 @@ function loopComments(reddit, reply = false) {
     }
 }
 
-if(document.location.href.indexOf("?reddit") !== -1) {
-	document.location = 'https://redd.it/' + redditid;
+if (document.location.href.indexOf("?reddit") !== -1) {
+    document.location = 'https://redd.it/' + redditid;
 }
 
-document.getElementsByClassName("loader")[0].style.display = 'block';
-document.getElementsByClassName("noscript")[0].outerHTML='';
-
 window.addEventListener('load', function() {
-	reddit.comments(redditid, "trumptracker").limit(20).sort("hot").fetch(function(res) {
+    $.get("http://www.reddit.com/r/trumptracker/comments/" + redditid + ".json", function(res) {
         res.shift();
         loopComments(res);
-        if ($(".panel-group").html().indexOf('<div class="panel ')) {
-            $(".loader").hide();
-            $("#reddit_comments").append("<h4>There are no comments yet</h4>");
-        }
+        setTimeout(function() {
+            if (nocomments) {
+                $(".loader").hide();
+                $("#reddit_comments").append("<h4>There are no comments yet</h4>");
+            }
+        }, 500);
+    }).fail(function() {
+        $(".loader").hide();
+        $(".noscript").show();
     });
 });
